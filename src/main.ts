903d13d30897cdd647f0cec5e48d47a0c6f4508f
Jason Cutler <jamestyberius1@gmail.com>
2024-12-03 15:50:56 -0500

    player movement

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { makePlayer } from "./entities";
+import { makePlayer, setControls } from "./entities";
 import { k } from "./kaboomCtx";
 import { makeMap } from "./utils";
 
@@ -42,11 +42,12 @@ async function gameSetup() {
       level1SpawnPoints.player[0].y
     );
 
+    setControls(k, kirb);
     k.add(kirb);
     k.camScale(k.vec2(0.7));
     k.onUpdate(() => {
       if (kirb.pos.x < level1Layout.pos.x + 432)
-        k.camPos(kirb.pos.x + 500, 800);
+        k.camPos(kirb.pos.x + 500, 870);
     });
   });
 
